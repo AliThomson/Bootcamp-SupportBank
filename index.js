acbad9167ab530b9f508f9b8fe169f1c0a852bce
Alison Thomson <Alison.Thomson@softwire.com>
2022-04-21 16:26:33 +0100

    Add command line input

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 const csvToObj = require('csv-to-js-parser').csvToObj;
+const readlineSync = require('readline-sync');
+
+// Wait for user's response.
+
 
 const accountData = fs.readFileSync('Transactions2014.csv').toString();
 let obj = csvToObj(accountData);
@@ -53,10 +57,31 @@ bank.forEach(transaction =>
             }
     });
 
-        for (const key of Object.keys(bank)) {
-                console.log(key, bank[key]);
-        }
-        for (const key of Object.keys(accounts)) {
+        function printAll() {
+            for (const key of Object.keys(accounts)) {
                 console.log(key, accounts[key]);
+            }
         }
+        function printAccount(holder) {
+            let fromAccounts = bank.filter(transaction => transaction.from === holder);
+            for (const key of Object.keys(fromAccounts)) {
+                console.log(key, fromAccounts[key]);
+            }
+            let toAccounts = bank.filter(transaction => transaction.to === holder);
+            for (const key of Object.keys(toAccounts)) {
+                console.log(key, toAccounts[key]);
+            }
+        }
+let option = readlineSync.question('What would you like to output? List All or List [Account holder]');
+if (option === "List All") {
+    printAll();
+} else {
+    let holder = option.substring(5);
+    if (accounts.some(account => account['holder'] === holder)) {
+        printAccount(holder);
+    } else {
+        console.log("Please enter a valid account holder");
+    }
+}
+
 
