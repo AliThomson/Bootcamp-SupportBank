49500b84ce1682199c1c346ec1c9490fe81f0ec4
Alison Thomson <Alison.Thomson@softwire.com>
2022-04-22 11:01:06 +0100

    Add date and amount checks

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const csvToObj = require('csv-to-js-parser').csvToObj;
 const readlineSync = require('readline-sync');
 const log4js = require('log4js');
-const logger = log4js.getLogger('<filename>');
+const logger = log4js.getLogger('dodgyFileName');
+const format = require('date-fns/format');
 
 log4js.configure({
     appenders: {
@@ -44,11 +45,28 @@ class Account {
 let bank = [];
 try {
     for (let i = 0; i < obj.length; i++) {
-        bank.push(new Bank((obj[i]['Date']), obj[i]['From'], obj[i]['To'], obj[i]['Narrative'], parseFloat(obj[i]['Amount'])));
+        let amount = parseFloat(obj[i]['Amount'])
+        if (isNaN(amount)) {
+            logger.error(`Amount is not a number on ${obj[i]['Date']}: from ${obj[i]['From']}: to ${obj[i]['To']}`)
+        }
+        // let inputDate = format(new Date(obj[i]['Date']), 'yyyy-MM-dd')//Date.parse(obj[i]['Date']);
+        let inputDate  = obj[i]['Date'] //date string in dd/mm/yyyy format
+
+        let dateArray = inputDate.split("/");
+        let day = parseInt(dateArray[0], 10);
+        let month = parseInt(dateArray[1], 10)-1;
+        let year = parseInt(dateArray[2], 10);
+
+        let outputDate = new Date(year, month, day);
+
+        if (isNaN(outputDate)) {
+            logger.error(`Date is not valid(${obj[i]['Date']}): from ${obj[i]['From']}: to ${obj[i]['To']} narrative: ${obj[i]['Narrative']}`)
+        }
+        bank.push(new Bank((obj[i]['Date']), obj[i]['From'], obj[i]['To'], obj[i]['Narrative'], amount));
     }
 }
 catch(err) {
-    logger.error("Error loading bank")
+    logger.error(err)
 }
 let accounts = [];
 
